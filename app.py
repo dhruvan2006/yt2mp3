71262a05a83bf0cd7a9383705c4ae1d621306945
Dhruvan Gnanadhandayuthapani <dhruvan2006@gmail.com>
2024-11-02 19:10:39 +0100

    Refactored route templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template
 from pytubefix import YouTube
 import os
 
@@ -9,13 +9,7 @@ os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
 def index():
-    return '''
-    <form method="POST" action="/download">
-        <label for="url">YouTube URL:</label>
-        <input type="text" name="url" id="url" required>
-        <input type="submit" value="Download Audio">
-    </form>
-    '''
+    return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
 def download_audio():
@@ -34,11 +28,7 @@ def download_audio():
 
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
-        return render_template_string('''
-            <h1>Error</h1>
-            <p>{{ error_message }}</p>
-            <a href="/">Go back</a>
-        ''', error_message=error_message)
+        return render_template('error.html', error_message=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
